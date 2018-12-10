259eb8f1aa8a6954235a5461b14b8a201047cb9d
Alina Diez <alina.diezgr@gmail.com>
2018-12-09 20:36:20 -0600

    hola

--- routes.js
+++ routes.js
@@ -24,7 +24,7 @@ router.use((req, res, next) => {
             req.session.role = "usuario";
         }
     }
-    console.log(req.session);
+    //console.log(req.session);
     next();
 });
 
@@ -90,10 +90,173 @@ router.get("/logout", (req, res) => {
     res.redirect("/");
 });
 
+//debutante I
 router.get("/debutante-I",(req, res) => {
     res.render("debutante-I-examen");
 });
 
+router.post("/debutante-I", ensureAuthenticated,(req,res,next) => {
+//basado en lo de perfil de las otras practicas para agregar sobre un mismo usuario.
+    var A1 = req.body.A1.toLowerCase();
+    var B1 = req.body.B1.toLowerCase();
+    var C1 = req.body.C1.toLowerCase();
+    var A2 = req.body.A2;
+    var B2 = req.body.B2;
+    var C2 = req.body.C2;
+    var D2 = req.body.D2;
+    var A3 = req.body.A3.toLowerCase();
+    var B3 = req.body.B3.toLowerCase();
+    var C3 = req.body.C3.toLowerCase();
+    var D3 = req.body.D3.toLowerCase();
+    var A41 = req.body.A41.toLowerCase();
+    var A42 = req.body.A42.toLowerCase();
+    var A43 = req.body.A43.toLowerCase();
+    var B41 = req.body.B41.toLowerCase();
+    var B42 = req.body.B42.toLowerCase();
+    var B43 = req.body.B43.toLowerCase();
+    var A5 = req.body.A5;
+    var B5 = req.body.B5;
+    var C5 = req.body.C5;
+    var D5 = req.body.D5;
+    var A6 = req.body.A6.toLowerCase();
+    var B6 = req.body.B6.toLowerCase();
+    var C6 = req.body.C6.toLowerCase();
+    var D6 = req.body.D6.toLowerCase();
+    var A7 = req.body.A7;
+    var B7 = req.body.B7;
+    var C7 = req.body.C7;
+    var D7 = req.body.D7;
+    var A8 = req.body.A8;
+    var B8 = req.body.B8;
+    var C8 = req.body.C8;
+    var A9 = req.body.A9;
+    var B9 = req.body.B9;
+    var C9 = req.body.C9;
+    var deb1 = 0;
+    if(A1 === "mangent"){
+        deb1++;
+    }
+    if(B1 === "vous appelez"){
+        deb1++;
+    }
+    if(C1 === "chante"){
+        deb1++;
+    }
+    if(A2 === "suis"){
+        deb1++;
+    }
+    if(B2 === "a"){
+        deb1++;
+    }
+    if(C2 === "sommes"){
+        deb1++;
+    }
+    if(D2 === "ont"){
+        deb1++;
+    }
+    if(A3 === "anglais"){
+        deb1++;
+    }
+    if(B3 === "mexicaine"){
+        deb1++;
+    }
+    if(C3 === "espagnol"){
+        deb1++;
+    }
+    if(D3 === "américaine"){
+        deb1++;
+    }
+    if(A41 ==="quarante-neuf"){
+        deb1++;
+    }
+    if(A42 === "dix"){
+        deb1++;
+    }
+    if(A43 === "cinquante-neuf"){
+        deb1++;
+    }
+    if(B41 === "quatre-vingt-huit"){
+        deb1++;
+    }
+    if(B42 === "cent cinq"){
+        deb1++;
+    }
+    if(B43 === "cent quatre-vingt-treize"){
+        deb1++;
+    }
+    if(A5 ==="en"){
+        deb1++;
+    }
+    if(B5 === "au"){
+        deb1++;
+    }
+    if(C5 === "à"){
+        deb1++;
+    }
+    if(D5 === "aux"){
+        deb1++;
+    }
+    if(A6 === "votre"){
+        deb1++;
+    }
+    if(B6 === "ton"){
+        deb1++;
+    }
+    if(C6 === "notre"){
+        deb1++;
+    }
+    if(D6 === "son"){
+        deb1++;
+    }
+    if(A7 === "la"){
+        deb1++;
+    }
+    if(B7 === "des"){
+        deb1++;
+    }
+    if(C7 === "un"){
+        deb1++;
+    }
+    if(D7 === "une"){
+        deb1++;
+    }
+    if(A8 === "à côte dû"){
+        deb1++;
+    }
+    if(B8 === "à droite"){
+        deb1++;
+    }
+    if(C8 === "entre"){
+        deb1++;
+    }
+    if(A9 === "vos"){
+        deb1++;
+    }
+    if(B9 === "leur"){
+        deb1++;
+    }
+    if(C9 === "notre"){
+        deb1++;
+    }
+    console.log(deb1);
+    req.usuario.deb1 = deb1;
+    if(deb1 < 24){
+        req.usuario.deb1 = "Debutante I: Entre Nous 1 et 2";
+    }
+    req.usuario.save((err) => {
+        if(err){
+            next(err);
+            return;
+        }
+        res.redirect("/debutante-I-resultado");
+    });
+});
+
+//debutante I resultado
+router.get("/debutante-I-resultado", (req, res) => {
+    res.render("debutante-I-resultado");
+});
+    
 
 //listado de estudiantes
 router.get("/etudiantes", (req, res, next) =>{
